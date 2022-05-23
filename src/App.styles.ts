5b07776cdb0ec231bd4de2779e1e80f09a1946d6
Higa <codehiga@gmail.com>
2022-05-23 13:37:21 -0300

    initial commit

---
+++ src/App.styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #27282F;
+  color: #fff;
+  min-height: 100vh;
+`;
+
+export const Area = styled.div`
+  margin: auto;
+  max-width: 980px;
+  padding: 30px 0;
+`
+
+
+export const Header = styled.div`
+  width: 100%;
+  text-align: center;
+  margin-bottom: 30px;
+  
+`
